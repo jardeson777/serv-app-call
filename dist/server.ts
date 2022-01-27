0a911a843ce146e0dfa747836f7a8fca3a0ac988
jnogueira <jnogueira@marlin.com.br>
2022-01-27 09:47:58 -0300

    commit initial

---
+++ dist/server.ts
@@ -0,0 +1,62 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server, {
+  cors:{
+    origin:'*'
+  }
+});
+
+const users = {};
+
+const socketToRoom = {};
+
+io.on('connection', socket => {
+    socket.on("join room", roomID => {
+
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+        socket.emit("all users", usersInThisRoom);
+        console.log(usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
+});
+
+// const SERVER_PORT = 8000;
+// const SERVER_HOST = 'localhost';
+
+// server.listen(SERVER_PORT, SERVER_HOST, () => {
+//   console.log(`${SERVER_HOST}:${SERVER_PORT}`)
+// });
+server.listen(process.env.PORT || 8000, () => console.log('Servidor rodando'));
